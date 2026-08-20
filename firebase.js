75db1e2c0d394512fd0490acf2195634453b4715
Nhaithik Reddy Siddenki <nhaithikreddysiddenki@Nhaithiks-MacBook-Air.local>
2026-08-20 14:50:57 +0530

    Connect consultation history to Firestore

---
+++ firebase.js
@@ -0,0 +1,58 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  addDoc,
+  getDocs,
+  collection,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAk8sw1izMa86vAnCuLJuvuIDnQknYjaWQ",
+  authDomain: "mediscan-ai-f84e9.firebaseapp.com",
+  projectId: "mediscan-ai-f84e9",
+  storageBucket: "mediscan-ai-f84e9.firebasestorage.app",
+  messagingSenderId: "860555782619",
+  appId: "1:860555782619:web:77527f96cbd2a9bcee9ffe",
+  measurementId: "G-MFXKFX824X"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+
+export {
+  app,
+  auth,
+  db,
+
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+  signOut,
+
+  doc,
+  setDoc,
+  addDoc,
+  getDocs,
+  collection,
+  serverTimestamp
+};
